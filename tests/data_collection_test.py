1e0c270726f74b9307fde911089e1b79e7f948a6
Vagif <vagal2003@gmail.com>
2020-10-31 17:36:03 +0000

    added test to travis

--- tests/data_collection_test.py
+++ tests/data_collection_test.py
@@ -11,4 +11,6 @@ class TestQueries(unittest.TestCase):
         addresses = ['0x00009277775ac7d0d59eaad8fee3d10ac6c805e8','0x0002bda54cb772d040f779e88eb453cac0daa244']
         collector = DataCollector(inference=True)
         df = collector.main(inference_addresses=addresses)
+        
+        print(df)
         assert df != None
